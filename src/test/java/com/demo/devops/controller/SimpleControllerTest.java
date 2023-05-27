b1487c05f548638b217ae9d9310a8d2152417f30
root <root@MX2023>
2023-05-27 16:17:27 +0500

    add test case

---
+++ src/test/java/com/demo/devops/controller/SimpleControllerTest.java
@@ -0,0 +1,31 @@
+package com.demo.devops.controller;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class SimpleControllerTest {
+
+    @Test
+    void getAllCountriesTest() {
+        Assertions.assertEquals(2, 1*2);
+    }
+
+    @Test
+    void getAllCountriesTest1() {
+        Assertions.assertEquals(2, 1*2);
+    }
+
+    @Test
+    void getAllCountriesTest2() {
+        Assertions.assertEquals(2, 1*2);
+    }
+
+    @Test
+    void getAllCountriesTest3() {
+        Assertions.assertEquals(2, 1*2);
+    }
+}
